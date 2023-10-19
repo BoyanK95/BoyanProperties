ee1381022e546f1eddb4a3126c24ce2fd88b4e8a
BoyanK95 <boyan_k_95@abv.bg>
2023-10-19 23:40:04 +0300

    feat: connected initi server with mongoose

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.listen(3000, ()=> {
-    console.log('Server running on port 3000!');
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000!");
+});
