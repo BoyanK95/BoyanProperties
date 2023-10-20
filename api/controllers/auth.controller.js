eaa94eb12e7376c283789b4127bbb3168849539f
BoyanK95 <boyan_k_95@abv.bg>
2023-10-20 22:45:56 +0300

    fix: install bcrypt, use it to hash password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,17 @@
+import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
 
-  res.status(201).json("User created succesfully!");
+    res.status(201).json("User created succesfully!");
+  } catch (error) {
+    console.log(err);
+
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
