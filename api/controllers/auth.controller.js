3a34a53c140c18a393ef6a16843da5bd0bd1c966
BoyanK95 <boyan_k_95@abv.bg>
2023-10-22 00:25:15 +0300

    feat: create errorHandler func for trowing errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,6 +13,7 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created succesfully!");
   } catch (error) {
     // res.status(500).json(error.message);
-    next(error)
+    next(error);
+    // next(errorHandler(550, "error from the function!"));
   }
 };
