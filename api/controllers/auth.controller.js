5b782692c9022554fffaa2d306e12dc9270dffc5
BoyanK95 <boyan_k_95@abv.bg>
2023-10-21 10:39:34 +0300

    refactore: auth.controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,16 +2,16 @@ import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 
 export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
   try {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
 
     res.status(201).json("User created succesfully!");
   } catch (error) {
     console.log(err);
-
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json(error);
   }
 };
