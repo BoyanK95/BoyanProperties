4e85bab59da736daff0e35d52a2ff5ed669bee38
BoyanK95 <boyan_k_95@abv.bg>
2023-10-22 00:41:03 +0300

    feat: create sign up form page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <div>
+        <form action="POST" className="flex flex-col gap-4">
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            id="username"
+            className="border p-3 rounded-lg"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            id="email"
+            className="border p-3 rounded-lg"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            id="password"
+            className="border p-3 rounded-lg"
+          />
+          <button type="submit">SIGN UP</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
