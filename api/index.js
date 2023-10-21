fe5688ece5fc784c8ddb0bbecae636f08a37523f
BoyanK95 <boyan_k_95@abv.bg>
2023-10-21 23:24:10 +0300

    feat: create middleware for errors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 //config .env variables
 dotenv.config();
@@ -13,17 +13,27 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-
-  /** Creating server with express */
+/** Creating server with express */
 const app = express();
 
 /** Allowing json as input from server middleware */
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000!");
 });
 
-app.use('/api/user', userRouter)
+app.use("/api/user", userRouter);
 
-app.use('/api/auth', authRouter)
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
