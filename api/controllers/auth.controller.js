8d5d238238d1fc47e4b418b4fc83020bdd1da4a0
BoyanK95 <boyan_k_95@abv.bg>
2023-10-22 22:47:55 +0300

    fix: remove password from res

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -36,11 +36,13 @@ export const signin = async (req, res, next) => {
 
     /** User is correct, authenticating user (creating cookie) */
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass, ...rest} = validUser._doc
+
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json(validUser);
-      
+      .json(rest);
+
   } catch (error) {
     next(error);
   }
