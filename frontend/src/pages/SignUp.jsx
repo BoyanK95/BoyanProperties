ec4b4376a737eef0eb4ea6fd90ab1e3df7cc78c2
BoyanK95 <boyan_k_95@abv.bg>
2023-10-22 13:37:11 +0300

    fix: dissmis error on succesfull submit

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function SignUp() {
   const [formData, setFormData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
 
   const handleChange = (e) => {
     setFormData({
@@ -26,16 +26,15 @@ function SignUp() {
       });
 
       const data = await res.json();
-      // console.log(data);
-      console.log(error);
       if (data.success === false) {
         setIsLoading(false);
         setError(data.message);
+        console.log(error);
         return;
       }
       setIsLoading(false);
+      setError(null)
     } catch (error) {
-      // console.log(error);
       setIsLoading(false);
       setError(error.message);
     }
@@ -44,48 +43,46 @@ function SignUp() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <div>
-        <form onSubmit={onSubmit} className="flex flex-col gap-4">
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            id="username"
-            className="border p-3 rounded-lg"
-            onChange={handleChange}
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            id="email"
-            className="border p-3 rounded-lg"
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            id="password"
-            className="border p-3 rounded-lg"
-            onChange={handleChange}
-          />
-          <button
-            disabled={isLoading}
-            type="submit"
-            className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70"
-          >
-            {isLoading ? <p>Loading...</p> : <p>Sign up</p>}
-          </button>
-        </form>
-        {error && <p className="text-red-500">{error.message}</p>}
-      </div>
+      <form onSubmit={onSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          id="username"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          id="email"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          id="password"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={isLoading}
+          type="submit"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70"
+        >
+          {isLoading ? <p>Loading...</p> : <p>Sign up</p>}
+        </button>
+      </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error.message}</p>}
     </div>
   );
 }
