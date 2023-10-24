728a71d963668c113b94da1cb8588c01ceed1cee
BoyanK95 <boyan_k_95@abv.bg>
2023-10-24 10:16:34 +0300

    feat: configured redux store and create userSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js'
 
 export default configureStore({
-  reducer: {},
+  reducer: {user: userReducer},
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
