66adae4c3129434fa7516c6c733f3040f7a61707
BoyanK95 <boyan_k_95@abv.bg>
2023-11-05 19:35:22 +0200

    feat: create ProfileAvatar component and 3 inputs in Profile component

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import ProfileAvatar from "../components/ProfileAvatar";
+import { useUserCtx } from "../context/userCtx";
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        {/* {userState.currentUser?.avatar ? (
+          <img
+            className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+            src={userState.currentUser.avatar}
+            alt="profile-picture"
+            onError={(e) => (e.target.src = autoProfilePicString)}
+          />
+        ) : (
+          <img
+            className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+            src={autoProfilePicString}
+            alt="aut-profile-picture"
+          />
+        )} */}
+        <ProfileAvatar />
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          className="border p-3 rounded-lg m-3"
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          className="border p-3 rounded-lg m-3"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          className="border p-3 rounded-lg m-3"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
