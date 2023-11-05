7c911195fcb4f083730a00bf096b379582e2cc95
BoyanK95 <boyan_k_95@abv.bg>
2023-11-05 20:15:42 +0200

    feat: create profile page UI

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,25 +1,10 @@
 import ProfileAvatar from "../components/ProfileAvatar";
-import { useUserCtx } from "../context/userCtx";
 
 function Profile() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        {/* {userState.currentUser?.avatar ? (
-          <img
-            className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-            src={userState.currentUser.avatar}
-            alt="profile-picture"
-            onError={(e) => (e.target.src = autoProfilePicString)}
-          />
-        ) : (
-          <img
-            className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-            src={autoProfilePicString}
-            alt="aut-profile-picture"
-          />
-        )} */}
         <ProfileAvatar />
         <input
           type="text"
@@ -39,7 +24,18 @@ function Profile() {
           placeholder="password"
           className="border p-3 rounded-lg m-3"
         />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase disabled:opacity-70 hover:opacity-90">
+          Update
+        </button>
       </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer font-bold hover:opacity-80">
+          Delete account
+        </span>
+        <span className="text-red-700 cursor-pointer font-bold hover:opacity-80">
+          Sign out
+        </span>
+      </div>
     </div>
   );
 }
