da0c1eaf6fdfe56a7a843a97e81b5237a6b2c3d5
BoyanK95 <boyan_k_95@abv.bg>
2023-11-09 00:25:33 +0200

    feat: create updateUser controller

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,9 +1,38 @@
-export const test = (req, res) => {
-    res.json({
-        message: 'Hello my World!'
-    })
-}
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import User from "../models/user.model.js";
 
-export const updateUser = (req, res, next) => {
-    
-}
+export const test = (req, res) => {
+  res.json({
+    message: "Hello my World!",
+  });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You can only update your own account"));
+  }
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updateUser;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
