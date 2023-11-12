0e95e17cb11b4c2a9d5d727394e71dccda07b37b
BoyanK95 <boyan_k_95@abv.bg>
2023-11-12 21:56:49 +0200

    feat: add deleteUser functions to ctx, create handleDeleteUser

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,10 +3,19 @@ import ProfileAvatar from "../components/ProfileAvatar";
 import { useUserCtx } from "../context/userCtx";
 
 function Profile() {
-  const { userState, updateStart, updateSuccess, updateFail } = useUserCtx();
+  const {
+    userState,
+    updateStart,
+    updateSuccess,
+    updateFail,
+    deleteUserStart,
+    deleteUserSuccess,
+    deleteUserFail,
+  } = useUserCtx();
+
   const currentUser = userState.currentUser;
   const [formData, setFormData] = useState({});
-  const [updatedSuccessfully, setUpdatedSuccessfully] = useState(false)
+  const [updatedSuccessfully, setUpdatedSuccessfully] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -16,13 +25,16 @@ function Profile() {
     e.preventDefault();
     try {
       updateStart();
-      const res = await fetch(`/api/user/update/${currentUser._id || currentUser._doc._id}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const res = await fetch(
+        `/api/user/update/${currentUser._id || currentUser._doc._id}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await res.json();
       if (data.success === false) {
@@ -30,9 +42,30 @@ function Profile() {
         return;
       }
       updateSuccess(data);
-      setUpdatedSuccessfully(true)
+      setUpdatedSuccessfully(true);
     } catch (error) {
       updateFail(error.message);
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      deleteUserStart();
+      const res = await fetch(
+        `/api/user/delete/${currentUser._id || currentUser._doc._id}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      const data = await res.json();
+      if (data.success === false) {
+        deleteUserFail(data.message);
+        return;
+      }
+      deleteUserSuccess();
+    } catch (error) {
+      deleteUserFail(error.message);
     }
   };
 
@@ -72,15 +105,22 @@ function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer font-bold hover:opacity-80">
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer font-bold hover:opacity-80"
+        >
           Delete account
         </span>
         <span className="text-red-700 cursor-pointer font-bold hover:opacity-80">
           Sign out
         </span>
       </div>
-      <p className="text-red-700 mt-5">{userState.error ?  userState.error : ''}</p>
-      <p className="text-green-700 mt-5">{updatedSuccessfully ? 'User updated succesfully!' : ''}</p>
+      <p className="text-red-700 mt-5">
+        {userState.error ? userState.error : ""}
+      </p>
+      <p className="text-green-700 mt-5">
+        {updatedSuccessfully ? "User updated succesfully!" : ""}
+      </p>
     </div>
   );
 }
