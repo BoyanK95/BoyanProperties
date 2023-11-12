9299aebdab9433023e047f3ce7dcff48e7e82650
BoyanK95 <boyan_k_95@abv.bg>
2023-11-12 20:46:54 +0200

    feat: create deleteUser route and controller

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -25,7 +25,7 @@ export const updateUser = async (req, res, next) => {
           username: req.body.username,
           email: req.body.email,
           password: req.body.password,
-          avatar: req.body.avatar
+          avatar: req.body.avatar,
         },
       },
       { new: true }
@@ -33,6 +33,19 @@ export const updateUser = async (req, res, next) => {
 
     const { password, ...rest } = updatedUser;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You can only delete your own account"));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
