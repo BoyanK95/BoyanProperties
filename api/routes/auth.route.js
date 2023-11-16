6750b428563749211ab1545262b8b38cb5776f74
BoyanK95 <boyan_k_95@abv.bg>
2023-11-16 23:51:48 +0200

    feat: create listing route, model, init controller

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,16 @@
-import expres from 'express'
-import { googleAuth, signOut, signin, signup } from '../controllers/auth.controller.js'
+import expres from "express";
+import {
+  googleAuth,
+  signOut,
+  signin,
+  signup,
+} from "../controllers/auth.controller.js";
 
-const router = expres.Router()
+const router = expres.Router();
 
-router.post('/signup', signup)
-router.post('/signin', signin)
-router.post('/google', googleAuth)
-router.get('/signout', signOut)
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/google", googleAuth);
+router.get("/signout", signOut);
 
 export default router;
