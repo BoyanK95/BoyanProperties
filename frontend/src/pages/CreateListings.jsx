d3b368d925ff7404c0de4e6a9376a12c0cd4f58f
BoyanK95 <boyan_k_95@abv.bg>
2023-12-03 01:55:14 +0200

    refactore: use ImageCard to display images

--- frontend/src/pages/CreateListings.jsx
+++ frontend/src/pages/CreateListings.jsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { app } from "../firebase";
 import { useUserCtx } from "../context/userCtx";
 import { useNavigate } from "react-router-dom";
+import ImageCard from "../components/ImageCard";
 
 function CreateListings() {
   const { userState } = useUserCtx();
@@ -88,13 +89,6 @@ function CreateListings() {
           });
         }
       );
-    });
-  };
-
-  const deleteUploadedImage = (url) => {
-    setFormData({
-      ...formData,
-      imageUrls: formData.imageUrls.filter((imgUrl) => imgUrl !== url),
     });
   };
 
@@ -353,23 +347,12 @@ function CreateListings() {
           </div>
           {formData.imageUrls?.length > 0 &&
             formData.imageUrls?.map((url, idx) => (
-              <div
-                className="flex justify-between p-3 hover:shadow-md rounded-lg"
+              <ImageCard
                 key={idx}
-              >
-                <img
-                  src={url}
-                  alt="apartment-image"
-                  className="w-20 h-20 object-cover rounded-lg"
-                />
-                <button
-                  onClick={() => deleteUploadedImage(url)}
-                  type="button"
-                  className="text-red-700 border p-5 border-spacing-6 border-red-600 font-bold uppercase rounded-lg hover:opacity-80 hover:bg-red-700 hover:text-white"
-                >
-                  Delete
-                </button>
-              </div>
+                formData={formData}
+                setFormData={setFormData}
+                imageUrl={url}
+              />
             ))}
           <button
             disabled={isFormLoading || isUploading}
