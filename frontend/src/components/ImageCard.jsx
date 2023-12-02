3f7c9a5c00ec297883d7d4ed3960936e0e4f0d05
BoyanK95 <boyan_k_95@abv.bg>
2023-12-03 01:55:14 +0200

    refactore: use ImageCard to display images

--- frontend/src/components/ImageCard.jsx
+++ frontend/src/components/ImageCard.jsx
@@ -1,25 +1,22 @@
 import PropTypes from "prop-types";
 
-const ImageCard = ({ formData, setFormData, url, idx }) => {
-  const deleteUploadedImage = (url) => {
+const ImageCard = ({ formData, setFormData, imageUrl }) => {
+  const deleteUploadedImage = (imageUrl) => {
     setFormData({
       ...formData,
-      imageUrls: formData.imageUrls.filter((imgUrl) => imgUrl !== url),
+      imageUrls: formData.imageUrls.filter((url) => url !== imageUrl),
     });
   };
 
   return (
-    <div
-      className="flex justify-between p-3 hover:shadow-md rounded-lg"
-      key={idx}
-    >
+    <div className="flex justify-between p-3 hover:shadow-md rounded-lg">
       <img
-        src={url}
+        src={imageUrl}
         alt="apartment-image"
         className="w-20 h-20 object-cover rounded-lg"
       />
       <button
-        onClick={() => deleteUploadedImage(url)}
+        onClick={() => deleteUploadedImage(imageUrl)}
         type="button"
         className="text-red-700 border p-5 border-spacing-6 border-red-600 font-bold uppercase rounded-lg hover:opacity-80 hover:bg-red-700 hover:text-white"
       >
@@ -32,8 +29,7 @@ const ImageCard = ({ formData, setFormData, url, idx }) => {
 ImageCard.propTypes = {
   formData: PropTypes.object.isRequired,
   setFormData: PropTypes.func.isRequired,
-  url: PropTypes.string.isRequired,
-  idx: PropTypes.number.isRequired,
+  imageUrl: PropTypes.string.isRequired,
 };
 
 export default ImageCard;
