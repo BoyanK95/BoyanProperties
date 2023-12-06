fb55427c496e1a1f2ba9e06a1005d0332dddcbca
BoyanK95 <boyan_k_95@abv.bg>
2023-12-07 01:56:57 +0200

    fix: navigation when submiting editing listing

--- frontend/src/pages/UpdateListings.jsx
+++ frontend/src/pages/UpdateListings.jsx
@@ -62,7 +62,7 @@ function UpdateListings() {
         setFormError(data.message);
       }
 
-      navigate(`/listings/${data._id}`);
+      navigate(`/listing/${data._id}`);
     } catch (error) {
       setFormError(error.message);
       setIsFormLoading(false);
