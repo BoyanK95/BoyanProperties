1797a81b33e0ca9ebb70bf7380249db013df231c
BoyanK95 <boyan_k_95@abv.bg>
2023-12-07 18:52:44 +0200

    feat: create states and fetchListing logic

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -1,9 +1,37 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Listing = () => {
-  return (
-    <div>Listing</div>
-  )
-}
+  const [listing, setListing] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
-export default Listing
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        console.log(data);
+        if (data.success === false) {
+          setHasError(true);
+          setIsLoading(false);
+          return;
+        }
+        setIsLoading(false);
+        setListing(data);
+        setHasError(false);
+      } catch (error) {
+        setHasError(true);
+        setIsLoading(false);
+      }
+    };
+    fetchListing();
+  }, []);
+
+  return <div>Listing</div>;
+};
+
+export default Listing;
