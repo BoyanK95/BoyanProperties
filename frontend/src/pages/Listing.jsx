c1e0cdddab19b2419308e017d1483f4170ef3e6c
BoyanK95 <boyan_k_95@abv.bg>
2023-12-08 22:54:30 +0200

    feat: created coppy link state

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -22,6 +22,7 @@ const Listing = () => {
   const [listing, setListing] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
+  const [isCoppied, setIsCoppied] = useState(false);
 
   const params = useParams();
 
@@ -47,6 +48,14 @@ const Listing = () => {
   useEffect(() => {
     fetchListing();
   }, [fetchListing]);
+
+  const handleCoppyLink = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setIsCoppied(true);
+    setTimeout(() => {
+      setIsCoppied(false);
+    }, 2000);
+  };
 
   const handleRetry = () => {
     setHasError(false);
@@ -80,9 +89,14 @@ const Listing = () => {
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
-            <FaShare />
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer hover:shadow-md">
+            <FaShare className="text-slate-500" onClick={handleCoppyLink} />
           </div>
+          {isCoppied && (
+            <p className="fixed top-[19%] right-[2%] z-10 rounded-md bg-slate-100 p-2">
+              Link coppied!
+            </p>
+          )}
         </>
       )}
     </main>
