4ad36620ca8645db2cdd8a3a42b84d9894bab896
BoyanK95 <boyan_k_95@abv.bg>
2023-12-09 22:56:09 +0200

    feat: create initial ContactSection component

---
+++ frontend/src/components/ContactLandlord/ContactSection.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const ContactSection = ({ listing }) => {
+  return <div></div>;
+};
+
+ContactSection.propTypes = {
+  listing: PropTypes.object.isRequired,
+};
+
+export default ContactSection;
