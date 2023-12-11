ecb53653b6d4a01b87e69bda63eebc55dfa1da5e
BoyanK95 <boyan_k_95@abv.bg>
2023-12-11 21:05:18 +0200

    feat: ContactSection fetching landlord

--- frontend/src/components/ContactLandlord/ContactSection.jsx
+++ frontend/src/components/ContactLandlord/ContactSection.jsx
@@ -1,7 +1,53 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 const ContactSection = ({ listing }) => {
-  return <div></div>;
+  const [landlord, setLandlord] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const fetchLandlord = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`/api/user/${listing.userRef}`);
+        const data = await res.json();
+        console.log(data);
+        setIsLoading(false);
+        if (data.success === false) {
+          setHasError(true);
+          return;
+        }
+        setLandlord(data);
+        setHasError(false);
+      } catch (error) {
+        setHasError(true);
+        setIsLoading(false);
+      }
+    };
+    fetchLandlord();
+  }, [listing.userRef]);
+  console.log("landlord", landlord);
+
+  return (
+    <>
+      {landlord && (
+        <div className="">
+          <p>
+            Contact{" "}
+            <span className="font-semibold text-slate-900">
+              {landlord.username}
+            </span>{" "}
+            for{" "}
+            <span className="font-semibold text-slate-900">
+              {listing.name.toLowerCase()}
+            </span>
+          </p>
+          <textarea name="message" id="message" rows="3"></textarea>
+        </div>
+      )}
+    </>
+  );
 };
 
 ContactSection.propTypes = {
