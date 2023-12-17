85e23fe803e87b46245ebcf97a0aa36f6159b4d1
BoyanK95 <boyan_k_95@abv.bg>
2023-12-17 15:04:17 +0200

    feat: create searchTerm onChange state

--- frontend/src/components/Searchbar/Searchbar.jsx
+++ frontend/src/components/Searchbar/Searchbar.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const Searchbar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  console.log(searchTerm);
   return (
     <form className="bg-slate-100 p-3 rounded-lg flex items-center">
       <input
         className="bg-transparent focus:outline-none w-full sm:64"
+        onChange={(e) => setSearchTerm(e.target.value)}
         type="text"
         placeholder="Search..."
       />
