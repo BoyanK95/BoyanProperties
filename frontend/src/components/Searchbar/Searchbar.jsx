3aab108a771602a3f82cae53512b3ef6574246f0
BoyanK95 <boyan_k_95@abv.bg>
2023-12-17 19:16:14 +0200

    feat: create handleSubmit to navigate to search link

--- frontend/src/components/Searchbar/Searchbar.jsx
+++ frontend/src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Searchbar = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
   console.log(searchTerm);
   return (
-    <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+    <form
+      className="bg-slate-100 p-3 rounded-lg flex items-center"
+      onSubmit={handleSubmit}
+    >
       <input
         className="bg-transparent focus:outline-none w-full sm:64"
         onChange={(e) => setSearchTerm(e.target.value)}
