327fc91e3c4ec27cecbda773e2529a0c155fab1d
BoyanK95 <boyan_k_95@abv.bg>
2023-12-23 01:30:58 +0200

    feat: display listing img or defaultHouseListingPic add Link to it

--- frontend/src/components/SearchListing/SearchListingCard.jsx
+++ frontend/src/components/SearchListing/SearchListingCard.jsx
@@ -1,7 +1,19 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { defaultHousePic } from "../../assets/defaultHouseListingPic";
 
 const SearchListingCard = ({ listing }) => {
-  return <p>{listing.name}</p>;
+  return (
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+      <Link to={`/listing/${listing._id}`}>
+        <img
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-110 trnasition-scale duration-300"
+          src={listing.imageUrls[0] || defaultHousePic}
+          alt="listing-cover"
+        />
+      </Link>
+    </div>
+  );
 };
 
 SearchListingCard.propTypes = {
