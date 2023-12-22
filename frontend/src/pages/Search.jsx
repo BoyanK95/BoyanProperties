87deba90f1112e8f17153b93eabe24235cfcd9c1
BoyanK95 <boyan_k_95@abv.bg>
2023-12-22 16:06:41 +0200

    refactor: ErrorState to show on whole SearchPage

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -5,15 +5,27 @@ import ErrorState from "../components/ErrorState/ErrorState";
 const Search = () => {
   const [hasError, setHasError] = useState(false);
   console.log(hasError);
+
+  const handleRetry = () => {
+    
+  }
   return (
     <main className="flex flex-col md:flex-row">
-      <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
-        <SearchForm setHasError={setHasError} />
-      </div>
-      <div className="text-3xl font-semibold border-b p-3 text-slate-700 text-center items-center mt-2">
-        {hasError && <ErrorState />}
-        {!hasError && <h1>Listing results:</h1>}
-      </div>
+      {hasError && (
+        <div className="flex items-center justify-center w-full h-full mt-7">
+          <ErrorState handleRetry={handleRetry} />
+        </div>
+      )}
+      {!hasError && (
+        <>
+          <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
+            <SearchForm setHasError={setHasError} />
+          </div>
+          <div className="text-3xl font-semibold border-b p-3 text-slate-700 text-center items-center mt-2">
+            <h1>Listing results:</h1>
+          </div>
+        </>
+      )}
     </main>
   );
 };
