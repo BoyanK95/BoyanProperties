0cd068f4a5329694acf454c5ad181aeef5768352
BoyanK95 <boyan_k_95@abv.bg>
2023-12-25 23:00:46 +0200

    feat: create Listings component to display listings array

---
+++ frontend/src/components/HomeComponents/Listings.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+// import SearchListingCard from "../components/SearchListing/SearchListingCard";
+import PropTypes from "prop-types";
+import SearchListingCard from "../SearchListing/SearchListingCard";
+
+const Listings = ({ listingsArr, listingsHeader, listingsLinkContent }) => {
+  return (
+    <div>
+      <div className="my-3">
+        <h2 className="text-2xl font-semibold text-slate-600">
+          {listingsHeader}
+        </h2>
+        <Link
+          className="text-sm text-blue-700 hover:underline"
+          to={`/search?offer=true`}
+        >
+          {listingsLinkContent}
+        </Link>
+      </div>
+      <div className="flex flex-wrap gap-4">
+        {listingsArr.map((listing) => (
+          <SearchListingCard key={listing._id} listing={listing} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+Listings.propTypes = {
+  listingsArr: PropTypes.array.isRequired,
+  listingsHeader: PropTypes.string,
+  listingsLinkContent: PropTypes.string,
+};
+
+export default Listings;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,39 +2,76 @@ import { useEffect, useState } from "react";
 import TopHomeComponent from "../components/HomeComponents/TopHomeComponent";
 import { useUserCtx } from "../context/userCtx";
 import SwiperComponent from "../components/SwiperComponent/SwiperComponent";
+import SearchListingCard from "../components/SearchListing/SearchListingCard";
+import { Link } from "react-router-dom";
+import Listings from "../components/HomeComponents/Listings";
 
 function Home() {
   // const { userState } = useUserCtx();
   const [offerListings, setOfferListings] = useState([]);
-  const [offerListingsError, setOfferListingsError] = useState("");
   const [saleListings, setSaleListings] = useState([]);
-  const [saleListingsError, setSaleListingsErrpr] = useState("");
   const [rentListings, setRentListings] = useState([]);
-  const [rentListingsError, setRentListingsError] = useState("");
+  const [listingsError, setListingsError] = useState("");
 
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch(`/api/listing/search?offer=true&limit=4`);
+        const res = await fetch(`/api/listing/search?offer=true&limit=3`);
         const data = await res.json();
         console.log(data);
         if (data.success === false) {
-          setOfferListingsError(data.message);
+          setListingsError(data.message);
         }
         setOfferListings(data);
       } catch (error) {
-        setOfferListingsError(error);
+        setListingsError(error);
       }
     };
 
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch(`/api/listing/search?type=sale&limit=3`);
+        const data = await res.json();
+        console.log(data);
+        if (data.success === false) {
+          setListingsError(data.message);
+        }
+        setSaleListings(data);
+      } catch (error) {
+        setListingsError(error);
+      }
+    };
+
+    const fetchRentalListings = async () => {
+      try {
+        const res = await fetch(`/api/listing/search?type=rent&limit=3`);
+        const data = await res.json();
+        console.log(data);
+        if (data.success === false) {
+          setListingsError(data.message);
+        }
+        setRentListings(data);
+      } catch (error) {
+        setListingsError(error);
+      }
+    };
     fetchOfferListings();
   }, []);
-
   console.log(offerListings);
+
   return (
     <main>
       <TopHomeComponent />
       {offerListings && <SwiperComponent listingsArr={offerListings} />}
+      <div className="flex flex-col gap-8 my-10 p-3 mx-auto max-w-6xl">
+        {offerListings && offerListings.length > 0 && (
+          <Listings
+            listingsArr={offerListings}
+            listingsHeader={"Recent offers"}
+            listingsLinkContent={"Show more offers"}
+          />
+        )}
+      </div>
     </main>
   );
 }
