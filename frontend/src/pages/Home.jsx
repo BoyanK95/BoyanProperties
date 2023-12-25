2e5cc239cb20be2c81ecfdd60ddd450281bc3c95
BoyanK95 <boyan_k_95@abv.bg>
2023-12-25 11:38:28 +0200

    refactor: Swiper component to hold listingsArr

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react";
 import TopHomeComponent from "../components/HomeComponents/TopHomeComponent";
 import { useUserCtx } from "../context/userCtx";
+import SwiperComponent from "../components/SwiperComponent/SwiperComponent";
 
 function Home() {
-  const { userState } = useUserCtx();
+  // const { userState } = useUserCtx();
+  const [offerListings, setOfferListings] = useState([]);
+  const [offerListingsError, setOfferListingsError] = useState("");
+  const [saleListings, setSaleListings] = useState([]);
+  const [saleListingsError, setSaleListingsErrpr] = useState("");
+  const [rentListings, setRentListings] = useState([]);
+  const [rentListingsError, setRentListingsError] = useState("");
 
-  console.log(userState);
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch(`/api/listing/search?offer=true&limit=4`);
+        const data = await res.json();
+        console.log(data);
+        if (data.success === false) {
+          setOfferListingsError(data.message);
+        }
+        setOfferListings(data);
+      } catch (error) {
+        setOfferListingsError(error);
+      }
+    };
+
+    fetchOfferListings();
+  }, []);
+
+  console.log(offerListings);
   return (
     <main>
       <TopHomeComponent />
+      {offerListings && <SwiperComponent listingsArr={offerListings} />}
     </main>
   );
 }
