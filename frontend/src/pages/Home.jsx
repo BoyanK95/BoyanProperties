6a843b2517b876e06af7e0bbd6a6179cb42bf73c
BoyanK95 <boyan_k_95@abv.bg>
2023-12-27 03:16:23 +0200

    feat: create offerListingsLoading state

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,25 +2,31 @@ import { useEffect, useState } from "react";
 import TopHomeComponent from "../components/HomeComponents/TopHomeComponent";
 import SwiperComponent from "../components/SwiperComponent/SwiperComponent";
 import Listings from "../components/HomeComponents/Listings";
+import ErrorState from "../components/ErrorState/ErrorState";
+import SkeletonSwiper from "../components/LoaderIcons/SkeletonSwiper";
 
 function Home() {
   const [offerListings, setOfferListings] = useState([]);
+  const [offerListingsLoading, setOfferListingsLoading] = useState(false);
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
   const [listingsError, setListingsError] = useState("");
 
   useEffect(() => {
     const fetchOfferListings = async () => {
+      setOfferListingsLoading(true);
       try {
         const res = await fetch(`/api/listing/search?offer=true&limit=3`);
         const data = await res.json();
         console.log(data);
+        setOfferListingsLoading(false);
         if (data.success === false) {
           setListingsError(data.message);
         }
         setOfferListings(data);
       } catch (error) {
         setListingsError(error);
+        setOfferListingsLoading(false);
       }
     };
 
@@ -56,21 +62,34 @@ function Home() {
     fetchSaleListings();
   }, []);
   console.log(offerListings);
+  console.log(offerListingsLoading);
+
+  const handleRetry = () => {
+    window.location.reload();
+    setListingsError("");
+    setOfferListingsLoading(false);
+  };
 
   return (
     <main>
       <TopHomeComponent />
-      {offerListings && <SwiperComponent listingsArr={offerListings} />}
+      {offerListings && !offerListingsLoading && (
+        <SwiperComponent listingsArr={offerListings} />
+      )}
+      {offerListingsLoading && <SkeletonSwiper />}
       <div className="flex flex-col gap-8 my-10 p-3 mx-auto max-w-6xl">
-        {offerListings && offerListings.length > 0 && (
-          <Listings
-            listingsArr={offerListings}
-            listingsHeader={"Recent offers"}
-            listingLink={`/search?offer=true`}
-            listingsLinkContent={"Show more offers"}
-          />
-        )}
-        {saleListings && saleListings.length && (
+        {offerListings &&
+          !listingsError &&
+          !offerListingsLoading &&
+          offerListings.length > 0 && (
+            <Listings
+              listingsArr={offerListings}
+              listingsHeader={"Recent offers"}
+              listingLink={`/search?offer=true`}
+              listingsLinkContent={"Show more offers"}
+            />
+          )}
+        {saleListings && !listingsError && saleListings.length && (
           <Listings
             listingsArr={saleListings}
             listingsHeader={"Find a home to buy"}
@@ -78,7 +97,7 @@ function Home() {
             listingsLinkContent={"Show more homes for sale"}
           />
         )}
-        {rentListings && rentListings.length && (
+        {rentListings && !listingsError && rentListings.length && (
           <Listings
             listingsArr={rentListings}
             listingsHeader={"Find a good place to rent"}
@@ -86,6 +105,7 @@ function Home() {
             listingsLinkContent={"Show more properties for rent"}
           />
         )}
+        {listingsError && <ErrorState handleRetry={handleRetry} />}
       </div>
     </main>
   );
